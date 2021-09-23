3e0cf1b9caadb79d9185f6492502beb8899ca658
Anthony You <you.anthony@yahoo.com>
2021-09-23 19:33:56 +0200

    feat: adding user info panel

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -12,9 +12,15 @@ module Types
           null: false,
           description: "Returns a list of items in the martian library"
 
+    field :me, Types::UserType, null: true
+
     def items
       # Item.all ### too many queries for user
       Item.preload(:user) ### resolves N+1 queries, but not useful when we don't need user
+    end
+
+    def me
+      context[:current_user]
     end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,6 +3,7 @@ module Types
     field :id, ID, null: false
     field :first_name, String, null: false
     field :last_name, String, null: false
+    field :full_name, String, null: false
     field :email, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
