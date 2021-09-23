379a759f3c9b81918865ebe64ce2078231839b61
Anthony You <you.anthony@yahoo.com>
2021-09-23 02:25:40 +0200

    feat: create user type

--- app/graphql/types/item_type.rb
+++ app/graphql/types/item_type.rb
@@ -5,6 +5,7 @@ module Types
     field :description, String, null: true
     field :image_url, String, null: true
     field :user_id, Integer, null: false
+    field :user, Types::UserType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
